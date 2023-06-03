e4681cf1859c4b81e9e84e5991cfca4aa7477b54
Ronald Gerits <ronald.gerits@gmail.com>
2023-06-03 17:10:04 +0200

    Section 2

---
+++ JSCounterApp/index.js
@@ -0,0 +1,16 @@
+let count = 0;
+const countAddBtn = document.getElementById("add");
+const countSubtractBtn = document.getElementById("subtract");
+const countHeader = document.getElementById("count");
+
+countHeader.innerText = count;
+
+countAddBtn.addEventListener("click", () => {
+  count++;
+  countHeader.innerText = count;
+});
+
+countSubtractBtn.addEventListener("click", () => {
+  count--;
+  countHeader.innerText = count;
+});
