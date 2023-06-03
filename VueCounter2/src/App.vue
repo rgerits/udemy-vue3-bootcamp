f1fc0285e8c4e4c118329c1c7fa9505b309f1c83
Ronald Gerits <ronald.gerits@gmail.com>
2023-06-03 19:25:32 +0200

    Section 3

---
+++ VueCounter2/src/App.vue
@@ -0,0 +1,56 @@
+<script setup>
+import { ref } from "vue";
+const count = ref(0);
+
+const addToCount = () => {
+  count.value++;
+};
+
+const subtractFromCount = () => {
+  count.value--;
+};
+</script>
+
+<!-- <script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    addToCount() {
+      this.count++;
+    },
+    subtractFromCount() {
+      this.count--;
+    },
+  },
+}; -->
+<!-- </script> -->
+
+<template>
+  <main>
+    <div>
+      <h4>Current count is...</h4>
+      <h1>{{ count }}</h1>
+      <button @click="subtractFromCount()">-</button>
+      <button @click="addToCount()">+</button>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  background-color: aliceblue;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+div {
+  text-align: center;
+}
+</style>
